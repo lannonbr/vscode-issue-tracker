f15e8ae99b8184ad996450ea40a5bfdfbabb7202
Benjamin Lannon <benjamin@lannonbr.com>
2018-09-11 20:53:05 -0400

    Showing daily diff above chart

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ db.collection("entries")
     let timesArr = [];
     let openIssueArr = [];
 
+    const nowEntry = entries[0].data();
+    const dayAgoEntry = entries[23].data();
+
     entries = entries.reverse();
 
     for (let entry of entries) {
@@ -70,4 +73,28 @@ db.collection("entries")
         height: 400
       }
     });
+
+    let diff = nowEntry.openIssues - dayAgoEntry.openIssues;
+
+    let colorClass;
+
+    if (diff > 0) {
+      colorClass = "red";
+      symbol = "▲";
+    } else {
+      colorClass = "green";
+      symbol = "▼";
+    }
+
+    diff = Math.abs(diff);
+
+    const diffElement = document.getElementById("diff");
+
+    diffElement.className = "color-" + colorClass;
+
+    diffElement.innerText = `${symbol} ${diff} ${
+      colorClass === "red" ? "more" : "less"
+    } issues compared to a day ago (${dayAgoEntry.openIssues} to ${
+      nowEntry.openIssues
+    })`;
   });
