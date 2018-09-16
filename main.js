c25b56cb3bf98507f4327777dd9443e9799d7eab
Benjamin Lannon <benjamin@lannonbr.com>
2018-09-16 10:29:40 -0400

    Added 10-day chart

--- main.js
+++ main.js
@@ -14,61 +14,115 @@ const settings = {
 
 db.settings(settings);
 
-db.collection("entries")
-  .orderBy("timestamp", "desc")
-  .limit(3 * 24)
-  .get()
-  .then(querySnapshot => {
-    let entries = querySnapshot.docs;
+getData(3 * 24).then(entries => {
+  let timesArr = [];
+  let openIssueArr = [];
 
+  const nowEntry = entries[0].data();
+  const dayAgoEntry = entries[23].data();
+
+  entries = entries.reverse();
+
+  let now = moment.unix(entries[0].data().timestamp);
+  let nextDayOne = now
+    .add(1, "day")
+    .startOf("day")
+    .unix();
+  let nextDayTwo = now
+    .add(1, "days")
+    .startOf("day")
+    .unix();
+  let nextDayThree = now
+    .add(1, "days")
+    .startOf("day")
+    .unix();
+  let nextDayFour = now
+    .add(1, "days")
+    .startOf("day")
+    .unix();
+
+  const gridLines = [
+    {
+      value: nextDayOne,
+      text: moment.unix(nextDayOne).format("ddd, MMMM Do")
+      // position: "end"
+    },
+    {
+      value: nextDayTwo,
+      text: moment.unix(nextDayTwo).format("ddd, MMMM Do")
+      // position: "end"
+    },
+    {
+      value: nextDayThree,
+      text: moment.unix(nextDayThree).format("ddd, MMMM Do")
+      // position: "end"
+    },
+    {
+      value: nextDayFour,
+      text: moment.unix(nextDayFour).format("ddd, MMMM Do")
+      // position: "end"
+    }
+  ];
+
+  console.log(gridLines);
+
+  for (let entry of entries) {
+    const { timestamp, openIssues } = entry.data();
+
+    timesArr.push(timestamp);
+    openIssueArr.push(openIssues);
+  }
+
+  var chart = c3.generate({
+    bindto: "#hourChart",
+    data: {
+      x: "x",
+      columns: [["x", ...timesArr], ["open issues", ...openIssueArr]]
+    },
+    axis: {
+      y: {
+        tick: {
+          format: function(x) {
+            return x === Math.floor(x) ? x : "";
+          }
+        }
+      },
+      x: {
+        tick: {
+          format: function(d) {
+            return moment.unix(d).format("llll");
+          }
+        }
+      }
+    },
+    tooltip: {
+      format: {
+        title: function(d) {
+          return moment.unix(d).format("llll");
+        }
+      }
+    },
+    padding: {
+      right: 100,
+      left: 100
+    },
+    size: {
+      height: 400
+    },
+    grid: {
+      x: {
+        lines: gridLines
+      }
+    }
+  });
+
+  fillDiffs(nowEntry, dayAgoEntry);
+
+  getData(720).then(entries => {
     let timesArr = [];
     let openIssueArr = [];
 
-    const nowEntry = entries[0].data();
-    const dayAgoEntry = entries[23].data();
-
-    entries = entries.reverse();
-
-    let now = moment.unix(entries[0].data().timestamp);
-    let nextDayOne = now
-      .add(1, "day")
-      .startOf("day")
-      .unix();
-    let nextDayTwo = now
-      .add(1, "days")
-      .startOf("day")
-      .unix();
-    let nextDayThree = now
-      .add(1, "days")
-      .startOf("day")
-      .unix();
-    let nextDayFour = now
-      .add(1, "days")
-      .startOf("day")
-      .unix();
-
-    const gridLines = [];
-
-    gridLines.push({
-      value: nextDayOne,
-      text: moment.unix(nextDayOne).format("ddd, MMMM Do"),
-      position: "end"
-    });
-    gridLines.push({
-      value: nextDayTwo,
-      text: moment.unix(nextDayTwo).format("ddd, MMMM Do"),
-      position: "end"
-    });
-    gridLines.push({
-      value: nextDayThree,
-      text: moment.unix(nextDayThree).format("ddd, MMMM Do"),
-      position: "end"
-    });
-    gridLines.push({
-      value: nextDayFour,
-      text: moment.unix(nextDayFour).format("ddd, MMMM Do"),
-      position: "end"
-    });
+    entries = entries.reverse().filter((_, idx) => idx % 4 === 0);
 
     for (let entry of entries) {
       const { timestamp, openIssues } = entry.data();
@@ -78,7 +132,7 @@ db.collection("entries")
     }
 
     var chart = c3.generate({
-      bindto: "#chart",
+      bindto: "#tenDayChart",
       data: {
         x: "x",
         columns: [["x", ...timesArr], ["open issues", ...openIssueArr]]
@@ -112,42 +166,51 @@ db.collection("entries")
       },
       size: {
         height: 400
-      },
-      grid: {
-        x: {
-          lines: gridLines
-        }
       }
     });
-
-    let diff = nowEntry.openIssues - dayAgoEntry.openIssues;
-
-    let colorClass;
-
-    if (diff > 0) {
-      colorClass = "red";
-      symbol = "▲";
-    } else {
-      colorClass = "green";
-      symbol = "▼";
-    }
-
-    diff = Math.abs(diff);
-
-    const diffElement = document.getElementById("diff");
-
-    diffElement.className = "color-" + colorClass;
-
-    diffElement.innerText = `${symbol} ${diff} ${
-      colorClass === "red" ? "more" : "less"
-    } issues compared to a day ago (${dayAgoEntry.openIssues} to ${
-      nowEntry.openIssues
-    })`;
-
-    const totalDiffElement = document.getElementById("totalDiff");
-
-    totalDiffElement.innerHTML = `On Sept 10, 2018 at 8:41PM EST, There was 5299 issues, now there are ${
-      nowEntry.openIssues
-    } for a total difference of <b>${5299 -
-      nowEntry.openIssues} issues</b> since I started tracking this.`;
   });
+});
+
+function fillDiffs(nowEntry, dayAgoEntry) {
+  let diff = nowEntry.openIssues - dayAgoEntry.openIssues;
+  let colorClass;
+  let symbol;
+
+  if (diff > 0) {
+    colorClass = "red";
+    symbol = "▲";
+  } else {
+    colorClass = "green";
+    symbol = "▼";
+  }
+
+  diff = Math.abs(diff);
+
+  const diffElement = document.getElementById("diff");
+
+  diffElement.className = "color-" + colorClass;
+
+  diffElement.innerText = `${symbol} ${diff} ${
+    colorClass === "red" ? "more" : "less"
+  } issues compared to a day ago (${dayAgoEntry.openIssues} to ${
+    nowEntry.openIssues
+  })`;
+
+  const totalDiffElement = document.getElementById("totalDiff");
+
+  totalDiffElement.innerHTML = `
+    On Sept 10, 2018 at 8:41PM EST, There was 5299 issues, now there
+    are ${nowEntry.openIssues} for a total difference of
+    <b>${5299 - nowEntry.openIssues} issues</b> since I started tracking this.
+  `;
+}
+
+function getData(hours) {
+  return new Promise(resolve => {
+    db.collection("entries")
+      .orderBy("timestamp", "desc")
+      .limit(hours)
+      .get()
+      .then(querySnapshot => resolve(querySnapshot.docs));
+  });
+}
